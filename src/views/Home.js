bc5eecf0f6d23c1c0b932d4fc86cf6c81d1997ac
BonnieMilian <bonniemilian@galileo.edu>
2018-07-24 12:32:44 -0600

    Do login, on login right go to Gifs and send object user data

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
+import FacebookLoginButton from '../components/FacebookLoginButton';
 import '../App.css';
 
 export default class Home extends Component {
 
-  loginFB = () => {
-    this.props.history.push('/gifs');
+  onFacebookLogin = (loginStatus, resultObject) => {
+    console.log("loginStatus",loginStatus)
+    if (loginStatus === true) {
+      console.log("resultObject",resultObject)
+      this.props.history.push({
+        pathname: '/gifs',
+        state: { fbUser: resultObject.user }
+      });
+    } else {
+      console.log('Facebook login error');
+    }
   }
 
   render() {
     return (
       <div className="App">
         <h1>Welcome to GIF Find & Share</h1>
-        <img src="/img/fb_login.png" alt="login"
-          onClick={this.loginFB}
-        />
+        <FacebookLoginButton onLogin={this.onFacebookLogin}>
+          <img src="/img/fb_login.png" alt="Login with Facebook" />
+        </FacebookLoginButton>
       </div>
     );
   }
